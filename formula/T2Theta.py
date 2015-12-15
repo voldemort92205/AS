4dd553f0f91902b39ff79256a1a88a83218b1f49
b01902076 <b01902076@csie.ntu.edu.tw>
2015-12-15 20:49:02 +0800

    convert T to virtual temperature with reference pressure is 1000hPa

---
+++ formula/T2Theta.py
@@ -0,0 +1,23 @@
+import math
+
+####
+#	author: Perry Lee
+#	mail:	voldemort92205@gmail.com
+#	date:	2015/12/15
+####
+
+# parameters
+Rd = 287.0
+Cp = 1004.0
+T0 = 273.15
+P0 = 1000		# reference pressure
+
+def Tc2Theta (T, P):
+	# T [degree C], P [hPa]
+	Theta = (T+T0)* 1.0 * math.pow (1.0*P0/P, Rd/Cp)
+	return Theta
+
+def Tk2Theta (T, P):
+	# T [k], P [hPa]
+	Theta = T * 1.0 * math.pow (1.0*P0/P, Rd/Cp)
+	return Theta
